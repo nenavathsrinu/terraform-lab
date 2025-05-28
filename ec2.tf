2b0d48b585ce9f3b627232d12d721bb2adcc997a
srinu <root@ip-172-31-4-88.ap-south-1.compute.internal>
2025-05-28 10:35:51 +0000

    userdata

--- ec2.tf
+++ ec2.tf
@@ -1,36 +1,43 @@
- resource "aws_instance" "web" {
-    ami = "ami-06031e2c49c278c8f"
-    instance_type = "t2.micro"
-    key_name = "linux-test"
-    vpc_security_group_ids = [aws_security_group.web-sg.id]
-       tags = {
-         Name = "Webserver"
-         Env = "prod"
-       }
+resource "aws_instance" "web" {
+  ami                    = "ami-06031e2c49c278c8f"
+  instance_type          = "t2.micro"
+  key_name               = "linux-test"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  availability_zone      = "ap-south-1b"
 
+  tags = {
+    Name = "Webserver"
+    Env  = "prod"
+  }
 
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install httpd -y
+    echo "welcome to terraform classes" > /var/www/html/index.html
+    systemctl enable httpd
+    systemctl start httpd
+  EOF
 }
 
 resource "aws_security_group" "web-sg" {
-    vpc_id = "vpc-0bcc7ee7f5a6515fc"
-      tags = {
-        Name = "web-sg"
-        Env = "Prod"
-	Resource_owner = "srinu"
-        Map-migrated = "sri-sri"
-      }
+  vpc_id = "vpc-0bcc7ee7f5a6515fc"
+
+  tags = {
+    Name = "web-sg"
+    Env  = "Prod"
+  }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "Allow_all" {
-    security_group_id = aws_security_group.web-sg.id
-    ip_protocol = "-1"
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.web-sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "Allow_all" {
-    security_group_id = aws_security_group.web-sg.id
-    ip_protocol = "-1"
-    cidr_ipv4 = "0.0.0.0/0"
-
+  security_group_id = aws_security_group.web-sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
